4dd6d9c528c0763bd2ab1fedd4adbdb984749e0e
CarpentJ <jeremy.charpe@gmail.com>
2024-03-19 17:05:17 +0100

    mlanglace

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-alert("coucou");
+function signup() {
+    var username = document.getElementById("signup-username").value;
+    var password = document.getElementById("signup-password").value;
+  
+  
+    if (username && password) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+        alert("Signed up successfully!");
+    } else {
+        alert("Please fill all fields.");
+    }
+  }
+  
+  
+  function login() {
+    var username = document.getElementById("login-username").value;
+    var password = document.getElementById("login-password").value;
+  
+  
+    var storedUsername = localStorage.getItem("username");
+    var storedPassword = localStorage.getItem("password");
+  
+    /*fetch(){
+
+    }*/
+  
+    if (username === storedUsername && password === storedPassword) {
+        alert("Login successful!");
+    } else {
+        alert("Invalid username or password.");
+    }
+  }
+  
